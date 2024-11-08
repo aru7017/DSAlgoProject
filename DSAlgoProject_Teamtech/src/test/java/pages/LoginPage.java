be00c45bc9bd78963fd051b7094351b57b5f77bf
aruna6002 <arunabr6002@gmail.com>
2024-11-08 09:39:13 -0700

    Comitting login page

---
+++ DSAlgoProject_Teamtech/src/test/java/pages/LoginPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class LoginPage {
+
+public	By userNM = By.xpath("//*[@id='id_username']");
+public	By Pwd = By.xpath("//*[@id='id_password']");	
+public	By Lgbutton = By.xpath("//*[@type='submit']");	
+	
+	
+	
+}
