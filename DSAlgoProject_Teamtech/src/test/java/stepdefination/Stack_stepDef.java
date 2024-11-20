658e2121511c4f8ae86c733904ddbbfa3d5c647f
subhadra sahu <subhadrasahu12@gmail.com>
2024-11-19 23:47:05 -0500

    commiting

---
+++ DSAlgoProject_Teamtech/src/test/java/stepdefination/Stack_stepDef.java
@@ -0,0 +1,40 @@
+package stepdefination;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import pages.StackPage;
+
+public class Stack_stepDef {
+	   public WebDriver driver;
+	   public StackPage sp;
+	   StackPage page = new StackPage(driver);
+	@Given("User is in Stack Page")
+	public void user_is_in_stack_page() throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+	    driver.get("https://dsportalapp.herokuapp.com/home"); 
+	    page.ClickGetStaretd();
+	    Thread.sleep(1000);
+	}
+
+	@When("User clicks in Operations in Stack Page")
+	public void user_clicks_in_operations_in_stack_page() {
+	    
+	   
+	}
+
+	@Then("User should be navigated to Operations in Stack page with title {string}")
+	public void user_should_be_navigated_to_operations_in_stack_page_with_title(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+}
