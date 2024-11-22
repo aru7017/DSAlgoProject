51be349a985d92f130a8f86713f6e1446d02531f
aruna6002 <arunabr6002@gmail.com>
2024-11-22 12:19:27 -0700

    commiting new changes

--- DSAlgoProject_Teamtech/src/test/java/stepdefination/Login_stepDef.java
+++ DSAlgoProject_Teamtech/src/test/java/stepdefination/Login_stepDef.java
@@ -1,11 +1,8 @@
 package stepdefination;
 
-import org.openqa.selenium.By;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -13,92 +10,142 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import pages.LoginPage;
 
 public class Login_stepDef {
-public WebDriver driver ;
-
-LoginPage page = new LoginPage();
-/*	@Given("^User is on the Login Page$")
-    public void user_is_on_the_login_page() {
+	public WebDriver driver;
+	public LoginPage page ;
+	@Given("user is on the browser")
+	public void user_is_on_the_browser() {
 		WebDriverManager.edgedriver().setup();
 		WebDriver driver = new EdgeDriver();
-		driver.get("https://dsportalapp.herokuapp.com/login");
-		//driver.manage().window().minimize();
-		driver.findElement(By.xpath("//*[@id='id_username']")).sendKeys("techteam");
-		driver.findElement(By.xpath("//*[@id='id_password']")).sendKeys("Time4team$");
-		driver.findElement(By.xpath("//*[@type='submit']")).click();
+		page = new LoginPage(driver);
+		 driver.get("https://dsportalapp.herokuapp.com/login");
+		    driver.manage().window().maximize();
 	}
-	@Given("^User enters username and password$")
-    public void user_enters_username_and_password() {
-	//	driver.findElement(By.xpath("//*[@id='id_username']")).sendKeys("techteam");
-		//driver.findElement(By.xpath("//*[@id='id_password']")).sendKeys("Time4team$");
-	}
-	@Given("^User clicks on login button$")
-    public void user_clicks_on_login_button() {
-	//	driver.findElement(By.xpath("//*[@type='submit']")).click();
+
+	@When("user enters {string} and {string}")
+	public void user_enters_and(String string, String string2) {
+	  page.enterUserName();
+	  page.enterPassword();
 		
-	}*/
-/*@Given("user is on the browser")
-public void user_is_on_the_browser() {
+	}
+
+	@Then("user navigate to home page")
+	public void user_navigate_to_home_page() {
+	  
+		page.clickLgButton();
+		driver.close();
+		
+	}
+	
+	@Given("user is on the LoginPage")
+	public void user_is_on_the_login_page() {
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
+		page = new LoginPage(driver);
+		 driver.get("https://dsportalapp.herokuapp.com/login");
+		    driver.manage().window().maximize(); 
+	}
+
+	@When("user enters {string} and {string} all fields empty")
+	public void user_enters_and_all_fields_empty(String string, String string2) {
+	page.emptyUserName();
+	page.emptyPassword();
+	}
+
+	@Then("user navigate to homepage")
+	public void user_navigate_to_homepage() {
+	   page.clickLgButton();
+	}
+
+@Given("user navigate the Login Page")
+public void user_navigate_the_login_page() {
 	WebDriverManager.edgedriver().setup();
 	WebDriver driver = new EdgeDriver();
-driver.get("https://dsportalapp.herokuapp.com/login");
-
+	page = new LoginPage(driver);
+	 driver.get("https://dsportalapp.herokuapp.com/login");
+	    driver.manage().window().maximize(); 
 }
 
-@And("user is on the login page")
-public void user_is_on_the_login_page() {
-   
+@When("user enters {string} and {string} password only")
+public void user_enters_and_password_only(String string, String string2) {
+   page.emptyUserName();
+   page.enterPassword();
 }
 
-@When("^user enters (.*) and (.*)$")
-public void user_enters_username_and_password(String username,String password) {
-driver.findElement(By.xpath("//*[@id='id_username']")).sendKeys(username);
-driver.findElement(By.xpath("//*[@id='id_password']")).sendKeys(password);
-}
-@And("user is able to click on login button")
-public void user_is_able_to_click_on_login_button() {
-driver.findElement(By.xpath("//*[@type='submit']")).click();
-	
-	
+@Then("user navigates to homepage")
+public void user_navigates_to_homepage() {
+  page.clickLgButton();
 }
 
-@Then("user navigate to home page")
-public void user_navigate_to_home_page() {
-   
-}*/
-@Given("user is on the browser")
-public void user_is_on_the_browser() {
+@Given("user navigates the Login Page")
+public void user_navigates_the_login_page() {
 	WebDriverManager.edgedriver().setup();
 	WebDriver driver = new EdgeDriver();
-driver.get("https://dsportalapp.herokuapp.com/login"); 
-driver.close();
+	page = new LoginPage(driver);
+	 driver.get("https://dsportalapp.herokuapp.com/login");
+	    driver.manage().window().maximize();
 }
 
-@And("user is on the login page")
-public void user_is_on_the_login_page() {
+@When("user enters {string} and {string} username only")
+public void user_enters_and_username_only(String string, String string2) {
+  page.enterUserName();
+  page.emptyPassword();
+}
+
+@Then("user navigates to homepage successfully")
+public void user_navigates_to_homepage_successfully() {
+   page.clickLgButton();
+   
    
 }
-
-@When("^user enters \"(.*)\" and \"(.*)\"$")
-public void user_enters_and(String username, String password) {
+@Given("user is able to navigate Login Page")
+public void user_is_able_to_navigate_login_page() {
 	WebDriverManager.edgedriver().setup();
 	WebDriver driver = new EdgeDriver();
-driver.get("https://dsportalapp.herokuapp.com/login"); 
-	driver.findElement(page.userNM).sendKeys(username);
-	driver.findElement(page.Pwd).sendKeys(password); 
-	driver.findElement(page.Lgbutton).click();
-	driver.close();
+	page = new LoginPage(driver);
+	 driver.get("https://dsportalapp.herokuapp.com/login");
+	    driver.manage().window().maximize();
 }
 
-@And("user is able to click on login button")
-public void user_is_able_to_click_on_login_button() {
-	//driver.findElement(By.xpath("//*[@type='submit']")).click();
+@When("user enters {string} and \"Time4team$\"invalid username")
+public void user_enters_and_time4team$_invalid_username(String string) {
+   page.invalidUserName();
+   page.enterPassword();
 }
 
-@Then("user navigate to home page")
-public void user_navigate_to_home_page() {
-   
+@Then("user is able navigate to homepage")
+public void user_is_able_navigate_to_homepage() {
+   page.clickLgButton();
 }
 
 
+@Given("user is able to navigates LoginPage")
+public void user_is_able_to_navigates_login_page() {
+	WebDriverManager.edgedriver().setup();
+	WebDriver driver = new EdgeDriver();
+	page = new LoginPage(driver);
+	 driver.get("https://dsportalapp.herokuapp.com/login");
+	    driver.manage().window().maximize();
+}
+
+@When("user enters {string} and {string} invalid password")
+public void user_enters_and_invalid_password(String string, String string2) {
+	  page.enterUserName();
+	  page.invalidPassword();
+}
+
+@Then("user is able navigates to homepage successfull")
+public void user_is_able_navigates_to_homepage_successfull() {
+	  page.clickLgButton();
+}
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 }
--- DSAlgoProject_Teamtech/src/test/resources/features/login.feature
+++ DSAlgoProject_Teamtech/src/test/resources/features/login.feature
@@ -2,10 +2,53 @@ Feature: DsAlgo login feature
 
   Scenario Outline: Check login is successful with valid credentials
     Given user is on the browser
-    And user is on the login page
     When user enters "<username>" and "<password>"
-    And user is able to click on login button
     Then user navigate to home page
+
+    Examples: 
+      | username | password   |
+      | techteam | Time4team$ |
+
+  Scenario Outline: Check login feature with all empty fields
+    Given user is on the LoginPage
+    When user enters "<username>" and "<password>" all fields empty
+    Then user navigate to homepage
+
+    Examples: 
+      | username | password   |
+      | techteam | Time4team$ |
+
+  Scenario Outline: Check login feature with only entering the password field
+    Given user navigate the Login Page
+    When user enters "<username>" and "<password>" password only
+    Then user navigates to homepage
+
+    Examples: 
+      | username | password   |
+      | techteam | Time4team$ |
+
+  Scenario Outline: Check login feature with only entering the username field
+    Given user navigates the Login Page
+    When user enters "<username>" and "<password>" username only
+    Then user navigates to homepage successfully
+
+    Examples: 
+      | username | password   |
+      | techteam | Time4team$ |
+
+  Scenario Outline: Check login feature with invalid username field
+    Given user is able to navigate Login Page
+    When user enters "<username>" and "<password>"invalid username
+    Then user is able navigate to homepage
+
+    Examples: 
+      | username | password   |
+      | techteam | Time4team$ |
+
+  Scenario Outline: Check login feature with invalid password field
+    Given user is able to navigates LoginPage
+    When user enters "<username>" and "<password>" invalid password
+    Then user is able navigates to homepage successfull
 
     Examples: 
       | username | password   |
